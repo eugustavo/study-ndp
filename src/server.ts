528adf1b580c92130628b872d124006ca3c1b876
Gustavo Souza <iamgustavosouza@gmail.com>
2024-04-21 18:05:08 -0300

    chore: initial configs

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get('/', () => {
+  return 'Application for test create docker image and CI/CD with Github Actions';
+})
+
+app.listen({ port: 3333}).then(() => {
+  console.log("Server is running on port 3333");
+})
